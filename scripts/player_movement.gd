ee49246c65619faf7d5f2ea48828fb1470817566
Pepe Wightmore <pepejw@proton.me>
2025-08-09 12:34:26 +0100

    Implement Coyote Time

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -7,22 +7,23 @@ const MOVE_ACCEL = 0.10
 const STOP_ACCEL = 0.10
 var jumpTimer = 0.0
 var jumpVelocity = 0.0
-
+var coyote_timer = 0.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	
+	# jump starts here now
+	if is_on_floor():
+		coyote_timer = 0.0
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	# Handle jump.
+		coyote_timer += delta
 	if not Input.is_action_pressed("Jump"):
 		jumpTimer = 0.01
 		jumpVelocity = 0.0
 	if Input.is_action_just_pressed("Jump"):
 		if is_on_wall_only():
 			velocity.y = JUMP_VELOCITY*0.5
-		elif is_on_floor():
-			jumpVelocity = -4.8
+		elif coyote_timer < 0.1:
+			jumpVelocity = -4.2
 	if Input.is_action_pressed("Jump"):
 		jumpTimer += delta
 		if not is_on_wall_only() and jumpTimer < 0.2:
@@ -69,8 +70,20 @@ func _physics_process(delta: float) -> void:
 # implemented variable jump height
 #
 # Jumping on a ceiling gives height. This is not intended.
+
 # Jumping in mid-air gave height. Added is_on_floor() to is_action_just_pressed("Jump") and is_action_pressed("Jump")
+
 # Variable jumping no longer works as not on floor after jumping. Removing is_on_floor from is_action_pressed("Jump"). 
 # This works as the change in velocity.y is multiplied by jump_velocity, which is 0.0 unless jumped from floor
 # This broke wall jumping as it seems the infinite jump was causing it to work. 
 # Fixed by moving is_on_floor to elif after wall jump.
+
+# I think that jump height is too high. Lowering it.
+
+# Refer to line 67
+# How Coyote?
+# Timer, for frames after fall
+# is_on_floor, coyote_timer = 0.0
+# not is_on_floor, coyote_timer += delta
+# if coyote_timer < 10 or smtn jump
+# 10 is too many frames
